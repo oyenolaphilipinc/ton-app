6e9db6ec048dea5f8ae784f90e80339e03ea521e
Oyenola Philip <76276196+oyenolaphilipinc@users.noreply.github.com>
2025-07-09 07:07:51 +0100

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="text-center mt-8 text-gray-500">Copyright Speedyswap 2024</footer>
+    <footer className="text-center mt-8 text-gray-500">Copyright Speedyswap 2025</footer>
   )
 }
 
